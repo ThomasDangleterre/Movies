a134a069b04908393fa1723ebe84a21bb1d56367
Thomas <thomas@linux.home>
2020-07-18 22:17:32 +0200

    Init project & set up CI

---
+++ src/main/java/com/dangleterre/thomas/movies/MoviesApplication.java
@@ -0,0 +1,15 @@
+package com.dangleterre.thomas.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.dangleterre.thomas.movies.*")
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
